00b03272d0de30f41e2c37a075cd0e9407b114b5
Awadh-Awadh <awadhawadh84@yahoo.com>
2024-09-28 16:41:05 +0300

    add authorization

---
+++ Az204.MSALAuthorization/Constants/ApplicationConstants.cs
@@ -0,0 +1,7 @@
+namespace Az204.MSALAuthorization.Constants;
+
+public class ApplicationConstants
+{
+    public const string ClientId = "ClientIdFromAzure";
+    public const string TenantId = "TenantIdFromAzure";
+}
---
+++ Az204.MSALAuthorization/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Az204.MSALAuthorization.Constants;
+using Microsoft.Identity.Client;
+
+Console.WriteLine("Hello, World!");
+var app = PublicClientApplicationBuilder
+    .Create(ApplicationConstants.ClientId)
+    .WithAuthority(AzureCloudInstance.AzurePublic, ApplicationConstants.TenantId)
+    .WithRedirectUri("http://localhost")
+    .Build();
+    
+string[] scopes = ["user.read"];
+AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+Console.WriteLine($"Token: \t {result.AccessToken}");
